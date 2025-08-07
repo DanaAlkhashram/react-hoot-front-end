b1c87e1fa7e587f338895f7f7368c41326b25a78
Dana <danalkhashram@gmail.com>
2025-08-07 15:35:10 +0300

    almost

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -1,6 +1,25 @@
-c
-onst HootList = (props) => {
-  return <main>Hoot List</main>;
-};
+import { Link } from 'react-router-dom'
 
-export default HootList;
+const HootList = (props) => {
+
+    return (
+        <main>
+            <h1>Hoot List</h1>
+            {props.hoots.map((hoot) => (
+                <Link key={hoot._id} to={`/hoots/${hoot._id}`}>
+                    <article>
+                        <header>
+                            <h2>{hoot.title}</h2>
+                            <p>
+                                {hoot.author.username} posted on {new Date(hoot.createdAt).toLocaleDateString()}
+                            </p>
+                        </header>
+                        <p>{hoot.text}</p>
+                    </article>
+                </Link>
+            ))}
+        </main>
+    )
+}
+
+export default HootList
--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,14 +1,48 @@
-const BASE_URL = `${import.meta.env.VITE_EXPRESS_BACKEND_URL}/hoots`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`
 
 const index = async () => {
   try {
-    const res = await fetch(BASE_URL, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-    });
-    return res.json();
-  } catch (error) {
-    console.log(error);
+    const res = await fetch(BASE_URL)
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
   }
-};
+}
 
-export { index };
+const show = async (hootId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}`)
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const create = async (formData) => {
+  try {
+    const token = localStorage.getItem('token')
+
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(formData)
+    })
+
+    const data = await res.json()
+    return data
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export {
+  index,
+  show,
+  create,
+}
